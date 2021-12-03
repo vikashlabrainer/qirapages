378a19bc2a236636b523bbdceecc1c28b2c1aab5
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 04:01:15 +0530

    prepared header

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
+
 <!DOCTYPE html>
 
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</style>
-
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 <body>
-   
+<div id="container" class="header">
+ <img src="https://ftx-rp.netlify.app/img/ftx21.webp" class="logo"/>
+<button id="publish-story" style="float: right;">  </button>
+
+
+<div class="blurred-box" id="generatebox" style="display:none">
+<div class="back"></div>
+<div class="registration-form" style="z-index: 2;position: relative;text-align: center;">
+  <header style="color:white;padding:10px;">
+    <h1>Story, Ready?</h1>
+    <sub>Finish this last step and you're done.</sub>
+  </header>
+
+ 
+</div>
+  
+</div>
+
+<div id="success" class="blurred-box" title="Story is now live!" style="display:none">
+    
+</div>
+ 
+ 
+</div>
 </body>
 </html>
