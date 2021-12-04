61d7638e81d2bd2769d09923870afb8a68bb2b03
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 09:20:19 +0530

    PHP FUNCTIONS

---
+++ functions/function.php
@@ -0,0 +1,33 @@
+<?php
+
+function encrypt($string)
+{
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'VikashKumar'; // user define private key
+    $secret_iv = '5fgf5HJ5g27'; // user define secret key
+    $key = hash('sha256', $secret_key);
+    $iv = substr(hash('sha256', $secret_iv), 0, 16); // sha256 is hash_hmac_algo
+    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+    $output = base64_encode($output);
+    
+    return $output;
+}
+
+
+function decrypt($string)
+{
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'VikashKumar'; // user define private key
+    $secret_iv = '5fgf5HJ5g27'; // user define secret key
+    $key = hash('sha256', $secret_key);
+    $iv = substr(hash('sha256', $secret_iv), 0, 16); // sha256 is hash_hmac_algo
+	$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+    return $output;
+}
+
+
+
+
+
+
+?>
