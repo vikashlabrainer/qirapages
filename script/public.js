cb30d9be14705148e223e8785a2d9d47b10b6c75
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 09:23:45 +0530

    PUBLIC PAGE javacript

---
+++ script/public.js
@@ -0,0 +1,8 @@
+// Adding functionality to button of header
+  $("#publish-story").on("click", function() {
+      
+      $("#generatebox").fadeToggle( "fast");
+      $("#success").hide();
+    
+  });
+  // Ending functionality to button of header
