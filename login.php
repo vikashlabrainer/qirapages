7853520088a4a6eb220d8ad2f6b8d7f0ae68cea6
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 11:25:41 +0530

    Creating login page

---
+++ login.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+
+<style>
+.container{
+	
+	
+	    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: space-around;
+    align-items: center;
+    margin-top: 10%;
+    color: white;
+	
+}
+
+input {
+	
+    padding: 8px;
+    font-size: 20px;
+
+	
+}
+
+body{
+	
+	
+	background:black;
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="container">
+<img src="https://ftx-rp.netlify.app/img/ftx21.webp"/>
+<h1>Login to Qirapage</h1>
+<div>
+<sub>By default your OTP is 101010.</sub>
+</div>
+<div>
+<form id="frm1">
+<input type="text" name="mobile" id="mobile" placeholder="Mobile Number"/>
+<input type="submit" name="sub1" value="GET OTP"/>
+</form>
+
+</div>
+<div>
+
+</div>
+</div>
+<script>
+
+
+
+
+</script>
+</body>
+
+</html>
