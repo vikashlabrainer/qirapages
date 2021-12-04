ed794c29b9effe9f8b8720ff608e6bcfa9f2c3c1
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 16:13:29 +0530

    DEFINITIONS

---
+++ define.php
@@ -0,0 +1,23 @@
+<?php
+
+//table name
+define('TABLE_NAME', "ftx");
+//DEMO CAPTCHA SECRET KEY
+define('CAPTCHA_SECRET', "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe");
+
+//domain name ex: http://localhost or https://anonpe.com
+define('DOMAIN_NAME', "http://localhost");
+
+
+///RAZORPAY KEY
+define('RAZORPAY_KEY', ""); 
+
+define('RAZORPAY_SECRET', "");
+
+
+define('RAZORPAY_CURRENCY', "INR");
+
+
+define('TABLE_PAYMENTS', "ftx_payments");
+define('WITHDRAWAL_PAYMENTS', "withdrawals");
+?>
