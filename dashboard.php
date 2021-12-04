94340212f2f25ce1faa57fc27d9a1124e98e97ed
Vikash Kumar <vikash14609@gmail.com>
2021-12-04 13:24:35 +0530

    balance calculation added

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,24 @@
 <?php
-
 session_start();
+include __DIR__ .'/define.php';
+require __DIR__ .'/config/dbconnect.php';
+
 if(isset($_SESSION['logged'], $_SESSION['mobile'])){
 	
 	$mobile = $_SESSION['mobile'];
 	
+	$sql ="SELECT amount FROM ".TABLE_PAYMENTS." where seller='$mobile'";
+	$result = $conn->query($sql);
+	$rowresult = mysqli_query($conn, $sql);
+	$money = 0;
+        if ( $rowresult->num_rows > 0) {
+          
+             while ($rowsum = mysqli_fetch_assoc($rowresult)){
+                 $money +=  $rowsum['amount'];
+                 }
+		}
 	
+	 
 	?>
 	
 	
@@ -38,7 +51,7 @@ if(isset($_SESSION['logged'], $_SESSION['mobile'])){
 	
 	<div>
 	
-	<h1>Wallet Balance: </h1>
+	<h1>Wallet Balance: INR <?php echo $money?></h1>
 	
 	</div>
 	<div>
